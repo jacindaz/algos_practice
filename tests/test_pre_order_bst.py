2fd4d2066b8ae296dc329c20293abd6f32bd797e
Jacinda Zhong <jacinda.zhong@cloverhealth.com>
2019-04-07 14:19:18 -0700

    Add find_max unit test

--- tests/test_pre_order_bst.py
+++ tests/test_pre_order_bst.py
@@ -1,8 +1,7 @@
-import pre_order_bst as bst
+import algos.pre_order_bst as bst
 import pytest
 
 @pytest.mark.parametrize("tree,item_to_insert,expected", [
-
     pytest.param(
         [20, 10, 8, 25],
         22,
@@ -25,3 +24,13 @@ import pytest
 def test_bst_insert(tree, item_to_insert, expected):
     new_tree = bst.insert(tree, item_to_insert)
     assert new_tree == expected
+
+
+@pytest.mark.parametrize("tree,expected", [
+    ([20, 10, 8, 30], 30),
+    ([25, 0], 25),
+    ([20, 12, 10, 8], 20)
+])
+def test_bst_max(tree, expected):
+    actual_max = bst.find_max(tree)
+    assert actual_max == expected
