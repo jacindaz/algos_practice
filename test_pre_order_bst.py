2597fc88d996ffba781e979b9b6a527b2a5ab820
Jacinda Zhong <jacinda.zhong@cloverhealth.com>
2019-04-07 13:57:56 -0700

    More bst insertion test cases

--- test_pre_order_bst.py
+++ test_pre_order_bst.py
@@ -2,15 +2,24 @@ import pre_order_bst as bst
 import pytest
 
 @pytest.mark.parametrize("tree,item_to_insert,expected", [
-    (
+
+    pytest.param(
         [20, 10, 8, 25],
-        30,
-        [20, 10, 8, 25, 30],
+        22,
+        [20, 10, 8, 22, 25],
+        id="insert_middle_1"
     ),
-    (
+    pytest.param(
+        [20, 10, 8, 25],
+        12,
+        [20, 12, 10, 8, 25],
+        id="insert_middle_2"
+    ),
+    pytest.param(
         [25, 0],
         30,
-        [25, 0, 30]
+        [25, 0, 30],
+        id="insert_end"
     ),
 ])
 def test_bst_insert(tree, item_to_insert, expected):
