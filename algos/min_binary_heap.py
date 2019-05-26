9bb9405952ec4ce6a49adeb75a694f79bbd442fe
Jacinda Zhong <jacinda.zhong@cloverhealth.com>
2019-05-26 11:18:12 -0700

    Function to percolate node upwards until in the right spot in the tree

--- algos/min_binary_heap.py
+++ algos/min_binary_heap.py
@@ -57,9 +57,57 @@ def insert_node(new_item, existing_heap):
 #    5
 #  /   \
 # 9
-# print(insert(12, [0,5,9,11]))
-print(insert(11, [0,5,9]))
+# print(insert_node(12, [0,5,9,11]))
+# print(insert_node(11, [0,5,9]))
 # expected tree
 #    5
 #  /   \
 # 9    11
+
+
+# Insertion properties:
+#   => p: index of parent
+#   => left child: position 2p
+#   => right child: 2p + 1
+#   => find parent of node: n/2 (discard remainder)
+# Insertion steps:
+#   => insert the node from left -> right
+#   => percolate up
+
+# It is possible to write a method that will allow us
+# to regain the heap structure property by comparing the
+# newly added item with its parent. If the newly added item is
+# less than its parent, then we can swap the item with its parent.
+def percolate_node_up(existing_tree, new_item, new_item_index=None):
+    # find new_item's parent
+    if new_item_index is None:
+        new_item_index = len(existing_tree)-1
+
+    parent_index = int(new_item_index/2)
+    parent = existing_tree[parent_index]
+
+    print(f"tree: {existing_tree}")
+    print(f"new_item: {new_item}, new_item_index: {new_item_index}")
+    print(f"parent: {parent}, parent_index: {parent_index}")
+
+    if parent > new_item:
+        # swap parent and new_item
+        parent_to_swap = parent
+        existing_tree[parent_index] = new_item
+        existing_tree[new_item_index] = parent_to_swap
+
+        print("\n-------------swapped-------------")
+        print(f"tree: {existing_tree}")
+        print(f"new_item: {new_item}, new_item_index: {new_item_index}")
+        print(f"parent: {parent}, parent_index: {parent_index}")
+
+        percolate_node_up(existing_tree, new_item, parent_index)
+
+    else:
+        print("\n-------------recursion ends-------------")
+        print(f"existing_tree: {existing_tree}")
+        return existing_tree
+
+
+print(percolate_node_up([0,5,9,11,14,18,19,21,33,17,27], 7))
+# print(percolate_node_up([0,9,14,18,33,17,27,7], 7))
