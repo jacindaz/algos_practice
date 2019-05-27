0a195da85111835d70e0250bdc0ee20449065eca
Jacinda Zhong <jacinda.zhong@cloverhealth.com>
2019-05-27 10:00:29 -0700

    Insertion is just an append, fix up percolation, fill out test cases

--- algos/min_binary_heap.py
+++ algos/min_binary_heap.py
@@ -12,9 +12,6 @@ import ipdb
 #   => you fill in a complete binary tree w/ nodes from
 #      left to right
 
-min_heap1 = [0,5,9,11]
-min_heap2 = [0,5,9,11,14,18,19,21,33,17,27]
-
 # Insertion properties:
 #   => p: index of parent
 #   => left child: position 2p
@@ -24,62 +21,7 @@ min_heap2 = [0,5,9,11,14,18,19,21,33,17,27]
 #   => insert the node from left -> right
 #   => percolate up
 
-def insert_node(new_item, existing_heap):
-    max_index = len(existing_heap) - 1
-
-    # start at root
-    root_index = 1
-    root = existing_heap[root_index]
-    left_index = 2*(root_index)
-    right_index = 2*(root_index) + 1
-    print("\n=============\n")
-    print(f"existing_heap: {existing_heap}")
-    print(f"root_index: {root_index}, root: {root}")
-    print(f"left_index: {left_index}, right_index: {right_index}")
-    print("\n")
-
-    # if new item is larger than the root
-    # and the root doesn't have BOTH: a left and right child
-    if new_item > root and (left_index > max_index or right_index > max_index):
-        if left_index > max_index:
-            print("no left child node")
-
-        elif right_index > max_index:
-            print("no right child node")
-
-            # not sure if this always works?
-            # maybe need to grow array to the size of right_index
-            existing_heap.append(new_item)
-
-    return existing_heap
-
-# current tree:
-#    5
-#  /   \
-# 9
-# print(insert_node(12, [0,5,9,11]))
-# print(insert_node(11, [0,5,9]))
-# expected tree
-#    5
-#  /   \
-# 9    11
-
-
-# Insertion properties:
-#   => p: index of parent
-#   => left child: position 2p
-#   => right child: 2p + 1
-#   => find parent of node: n/2 (discard remainder)
-# Insertion steps:
-#   => insert the node from left -> right
-#   => percolate up
-
-# It is possible to write a method that will allow us
-# to regain the heap structure property by comparing the
-# newly added item with its parent. If the newly added item is
-# less than its parent, then we can swap the item with its parent.
 def percolate_node_up(existing_tree, new_item, new_item_index=None):
-    # find new_item's parent
     if new_item_index is None:
         new_item_index = len(existing_tree)-1
 
@@ -91,7 +33,6 @@ def percolate_node_up(existing_tree, new_item, new_item_index=None):
     print(f"parent: {parent}, parent_index: {parent_index}")
 
     if parent > new_item:
-        # swap parent and new_item
         parent_to_swap = parent
         existing_tree[parent_index] = new_item
         existing_tree[new_item_index] = parent_to_swap
@@ -103,11 +44,15 @@ def percolate_node_up(existing_tree, new_item, new_item_index=None):
 
         percolate_node_up(existing_tree, new_item, parent_index)
 
-    else:
-        print("\n-------------recursion ends-------------")
-        print(f"existing_tree: {existing_tree}")
-        return existing_tree
+    print("\n-------------recursion ends-------------")
+    print(f"existing_tree: {existing_tree}")
+    return existing_tree
 
 
-print(percolate_node_up([0,5,9,11,14,18,19,21,33,17,27], 7))
-# print(percolate_node_up([0,9,14,18,33,17,27,7], 7))
+def insert(existing_heap, new_item):
+    existing_heap.append(new_item)
+    final_tree = percolate_node_up(existing_heap, new_item)
+
+    print("\n-------------final tree-------------")
+    print(f"final_tree: {final_tree}")
+    return final_tree
---
+++ tests/test_min_binary_heap.py
@@ -0,0 +1,39 @@
+import algos.min_binary_heap as min_heap
+import pytest
+
+
+@pytest.mark.parametrize("existing_heap,new_item,expected", [
+    pytest.param(
+        [1],
+        2,
+        [1,2],
+        id="one_level_insert_2"
+    ),
+    pytest.param(
+        [0,5,9],
+        11,
+        [0,5,9,11],
+        id="two_levels_insert_11"
+    ),
+    pytest.param(
+        [0,6,7,12,10,15,17],
+        5,
+        [0,5,7,6,10,15,17,12],
+        id="three_levels_insert_5"
+    ),
+    pytest.param(
+        [0,5,9,11],
+        12,
+        [0,5,9,11,12],
+        id="three_levels_insert_12"
+    ),
+    pytest.param(
+        [0,9,14,18,33,17,27,7,40,41],
+        20,
+        [0,9,14,18,33,17,27,7,40,41,20],
+        id="four_levels_insert_20"
+    )
+])
+def test_insert(existing_heap, new_item, expected):
+    actual_heap = min_heap.insert(existing_heap, new_item)
+    assert actual_heap == expected
