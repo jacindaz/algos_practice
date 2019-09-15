f3d07d4a7a881616c88127b3d30f0a6260599acc
Jacinda Zhong <jacinda.zhong@cloverhealth.com>
2019-09-15 12:11:04 -0700

    Node insert_right and insert_left test

--- tests/test_trees.py
+++ tests/test_trees.py
@@ -1,5 +1,32 @@
 import pytest
-import algos.trees as trees
+from algos.trees import Node, insert_left
+
+
+def test_node():
+    root = Node(1)
+
+    leaf_left = Node(2)
+    root.left = leaf_left
+
+    leaf_right = Node(3)
+    root.right = leaf_right
+
+    # insert when left child exists
+    root.insert_left(Node(4))
+    assert root.left.value == 4
+    assert root.left.left.value == leaf_left.value
+
+    # insert when no left child
+    leaf_left.insert_left(Node(10))
+    leaf_left.left.value == 10
+
+    # insert when right child exists
+    root.insert_right(Node(12))
+    root.right.value == 12
+
+    # insert when no right child
+    leaf_right.insert_right(Node(20))
+    leaf_right.right.value == 20
 
 
 @pytest.mark.parametrize("root, child_value, expected_new_root", [
@@ -23,5 +50,5 @@ import algos.trees as trees
     )
 ])
 def test_insert_left(root, child_value, expected_new_root):
-    actual = trees.insert_left(root, child_value)
+    actual = insert_left(root, child_value)
     assert actual == expected_new_root
