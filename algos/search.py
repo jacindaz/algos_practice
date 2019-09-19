9bbd20f0ab7d439b52023cb66bdb5f6291393c10
Jacinda Zhong <jacinda.zhong@cloverhealth.com>
2019-09-19 12:53:22 -0700

    Different binary search solutions, from longest to shortest/best solution

--- algos/search.py
+++ algos/search.py
@@ -11,7 +11,7 @@ def sequential_search_ordered(ord_list, item_to_find):
 
     return False
 
-def binary_search(list, item_to_find, current_index=None):
+def binary_search_longest(list, item_to_find, current_index=None):
 
     if current_index is None:
         current_index = len(list) // 2
@@ -24,9 +24,50 @@ def binary_search(list, item_to_find, current_index=None):
         else:
             return False
 
+    print(f"\nlist: {list}")
+    print(f"item_to_find: {item_to_find}, current_index: {current_index}")
+    print(f"item_to_find {item_to_find} == {list[current_index]}")
+
     if list[current_index] == item_to_find:
         return True
     elif item_to_find > list[current_index]:
         return binary_search(list[current_index:], item_to_find, len(list[current_index:]) // 2)
     elif item_to_find < list[current_index]:
         return binary_search(list[:current_index], item_to_find, len(list[:current_index]) // 2)
+
+
+def binary_search_long(list_to_search, item_to_find, current_index=None):
+    current_index = (len(list_to_search)-1) // 2
+
+    if len(list_to_search) <= 2:
+        if item_to_find in list_to_search:
+            return True
+        else:
+            return False
+
+    print(f"\nlist to search: {list_to_search}")
+    print(f"item_to_find: {item_to_find}, current_index: {current_index}")
+    print(f"comparing {item_to_find} == {list_to_search[current_index]}")
+
+    if item_to_find == list_to_search[current_index]:
+        return True
+    elif item_to_find > list_to_search[current_index]:
+        return binary_search(list_to_search[current_index:], item_to_find)
+    elif item_to_find < list_to_search[current_index]:
+        return binary_search(list_to_search[:current_index], item_to_find)
+
+
+def binary_search(list_to_search, item_to_find, current_index=None):
+    current_index = len(list_to_search) // 2
+    if len(list_to_search) == 0:
+        return False
+
+    print(f"\nlist to search: {list_to_search}")
+    print(f"item_to_find: {item_to_find}, current_index: {current_index}")
+    print(f"comparing {item_to_find} == {list_to_search[current_index]}")
+
+    if item_to_find == list_to_search[current_index]:
+        return True
+    elif item_to_find > list_to_search[current_index]:
+        return binary_search(list_to_search[current_index+1:], item_to_find)
+    return binary_search(list_to_search[:current_index], item_to_find)
--- tests/test_search.py
+++ tests/test_search.py
@@ -17,8 +17,19 @@ def test_sequential_search_ordered(ordered_list, item_to_find, expected):
     ([1,2,3,4,10], 6, False),
     ([1,2,3], 3, True),
     ([1,2,3], 0, False),
-    ([1,2,3,4], 3, True)
+    ([1,2,3,4], 3, True),
+    ([0,1,2,8,13,17,19,32,42], 3, False),
+    ([0,1,2,8,13,17,19,32,42], 1, True),
+    ([0,1,2,8,13,17,19,32,42], 13, True),
+    ([0,1,2,8,13,17,19,32,42], 42, True),
+    ([0,1,2,8,13,17,19,32,42], 100, False),
+    ([0,1,2,8,13,17,19,32,42], -8, False),
 ])
 def test_binary_search(list, item_to_find, expected):
-    actual = search.binary_search(list, item_to_find)
-    assert actual == expected
+    actual1 = search.binary_search_longest(list, item_to_find)
+    actual2 = search.binary_search_long(list, item_to_find)
+    actual3 = search.binary_search(list, item_to_find)
+
+    assert actual1 == expected
+    assert actual2 == expected
+    assert actual3 == expected
