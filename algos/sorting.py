66ca13a3c0fa199a86a42cc9b465fdaaf4703fd7
Jacinda Zhong <jacinda.zhong@cloverhealth.com>
2019-09-24 10:50:39 -0700

    Remove comments/print statements, small refactor

--- algos/sorting.py
+++ algos/sorting.py
@@ -37,15 +37,9 @@ def selection_sort(my_list):
 def insertion_sort(my_list):
     max_index = len(my_list)-1
 
-    for current_index, current_item in enumerate(my_list):
-        current_item_index = current_index
-        previous_item_index = current_index-1
+    for current_item_index, current_item in enumerate(my_list):
+        previous_item_index = current_item_index-1
 
-        print("\n========================")
-        print(f"current_item_index: {current_item_index}, previous_item_index: {previous_item_index}")
-
-        # if finds an item that is not in the right place
-        # swaps with item before until it is in the right place
         current_item = my_list[current_item_index]
         previous_item = my_list[previous_item_index]
 
@@ -54,22 +48,12 @@ def insertion_sort(my_list):
             and current_item_index >= 1 \
             and current_item_index <= max_index:
 
-            print(f"\ncurrent_item: {current_item}, current_item_index: {current_item_index}")
-            print(f"previous_item: {previous_item}, previous_item_index: {previous_item_index}")
-
-            # swap
-            print(f"my_list before swap: {my_list}")
             my_list[current_item_index] = previous_item
             my_list[previous_item_index] = current_item
-            print(f"my_list after swap: {my_list}")
 
             current_item_index = previous_item_index
             previous_item_index -= 1
             current_item = my_list[current_item_index]
             previous_item = my_list[previous_item_index]
 
-        # then moves onto next item that is not in the right place
-
-        # it remembers which two items it checked last
-        # (so jumps to there after it puts previous item in the right place)
     return my_list
